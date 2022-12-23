6f5c36bc17e5de095819fb8b6362f0f2166cda06
Roney V. Silva <roneyvsilva@gmail.com>
2022-12-23 14:29:35 -0300

    BaseRepository.InsertAsync

--- src/Api.Domain/Interfaces/IRepository.cs
+++ src/Api.Domain/Interfaces/IRepository.cs
@@ -2,11 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 
 namespace Api.Domain.Interfaces
 {
-    public interface IRepository
+    public interface IRepository<T> where T : BaseEntity
     {
-        
+        Task<T> InsertAsync(T item);
+        Task<T> UpdateAsync(T item);
+        Task<bool> DeleteAsync(Guid id);
+        Task<T> SelectAsync(Guid id);
+        Task<IEnumerable<T>> SelectAsyc();
     }
 }
