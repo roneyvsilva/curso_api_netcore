20dae2e11127813b03e904e469b0d37335f9b7a0
Roney V. Silva <roneyvsilva@gmail.com>
2022-12-23 09:10:04 -0300

    Atualização

--- src/Api.Data/Context/ContextFactory.cs
+++ src/Api.Data/Context/ContextFactory.cs
@@ -7,7 +7,7 @@ namespace Api.Data.Context
     {
         public MyContext CreateDbContext(string[] args)
         {
-            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=MySql@root";
+            var connectionString = "Server=localhost;Port=3306;Database=dbAPI;Uid=root;Pwd=";
             var optionsBuilder = new DbContextOptionsBuilder<MyContext>();
             optionsBuilder.UseMySql(connectionString);
             return new MyContext(optionsBuilder.Options);
